ef3cdf3c34b0bd61a537720f820a55dca042fe47
Luis Alberto Siancas Retegui <112042262+DahuX04@users.noreply.github.com>
2025-10-04 17:26:07 -0500

    Add: Initial Project

---
+++ savingNow/src/main/java/com/qodara/savingNow/SavingNowApplication.java
@@ -0,0 +1,13 @@
+package com.qodara.savingNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingNowApplication.class, args);
+	}
+
+}
---
+++ savingNow/src/test/java/com/qodara/savingNow/SavingNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qodara.savingNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavingNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
